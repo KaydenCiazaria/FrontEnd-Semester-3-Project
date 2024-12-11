34e4ca792a32226074036fa0f21e0116c663eccf
ghjknehbor <troydevdan@gmail.com>
2024-12-11 17:11:05 +0700

    working get request

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,51 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import VillaCard from '../components/VillaCard/VillaCard';
 import './Home.css'; // Ensure this file contains styling for the grid
 
-const villas = [
-  {
-    image: '.././villa-image.png',
-    title: 'Beauvida Villa',
-    price: 'Rp. 200.000/Night',
-    rating: '4/5',
-    address: 'Jl. Prasopaca Raya no.20, Kebayoran Baru, Jakarta Selatan',
-    tags: ['Swimming Pool', '3 bedrooms', '2 toilets', 'BBQ']
-  },
-  {
-    image: '.././villa-image.png',
-    title: 'Seaside Retreat',
-    price: 'Rp. 300.000/Night',
-    rating: '5/5',
-    address: 'Jl. Pantai Indah, Bali',
-    tags: ['Sea View', '2 bedrooms', '1 toilet', 'Private Pool']
-  },
-  {
-    image: '.././villa-image.png',
-    title: 'Mountain Escape',
-    price: 'Rp. 250.000/Night',
-    rating: '4.5/5',
-    address: 'Jl. Puncak Sari, Bandung',
-    tags: ['Mountain View', '4 bedrooms', '3 toilets', 'Fireplace']
-  },
-  {
-    image: '.././villa-image.png',
-    title: 'Urban Oasis',
-    price: 'Rp. 400.000/Night',
-    rating: '4.8/5',
-    address: 'Jl. Merdeka Raya, Surabaya',
-    tags: ['City View', '3 bedrooms', '2 toilets', 'Gym Access']
-  },
-  {
-    image: '.././villa-image.png',
-    title: 'Very nice',
-    price: 'Rp. 450.000/Night',
-    rating: '3.6/5',
-    address: 'Jl. Yup, Surabaya',
-    tags: ['City View', '3 bedrooms', '2 toilets', 'Gym Access']
-  },
-];
-
 const Home = () => {
+
+  const [villas, setVillas] = useState([]);
+
+  useEffect(() => {
+
+    const fetchVillas = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/api/villa/show_villa');
+        const data = await response.json();
+
+        const transformedVillas = data.map(villa => ({
+          image: "",
+          title: villa.villa_name,
+          price: `${villa.price}/Night`,
+          rating: `${villa.review_rating}/5`,
+          address: villa.address,
+          tags: ['Whatever']
+        }));
+        setVillas(transformedVillas);
+      } catch (error) {
+        console.error('Error fetching villa data: ', error);
+      }
+    };
+
+    fetchVillas();
+  }, [])
+
   return (
     <div className="home-container">
       <h1>Welcome to Villa Rentals</h1>
