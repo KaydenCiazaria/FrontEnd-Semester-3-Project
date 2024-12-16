6dcb8cd410b5e1393955f30f425ab5f519b2402c
Bryant <bryanteffendi1234@gmail.com>
2024-12-16 13:49:24 +0700

    fixed form reservation dont touch it pls

--- src/components/Form/FormReservation.js
+++ src/components/Form/FormReservation.js
@@ -1,13 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import './Form.css';
 import './FormReservation.css';
 
 const FormReservation = () => {
   const [formData, setFormData] = useState({
-    name: "",
-    phoneNumber: "",
-    notes: "",
+    message: "",
   });
 
   const { id } = useParams();
@@ -21,90 +19,75 @@ const FormReservation = () => {
   };
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Prevents page reload
     const token = localStorage.getItem("jwtToken");
+
+    console.log(`Token: ${token}`);
 
     const useridend = "http://localhost:8080/api/users/auth/getusersId";
     const formend = "http://localhost:8080/api/reservation/create_reservation";
 
     try {
+
       const getuseid = await fetch(useridend, {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${token}`,
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
+          "Authorization": `Bearer ${token}`,
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        }
       });
 
       if (!getuseid.ok) {
         throw new Error(`Error fetching userid: ${getuseid.status}`);
-      }
+      };
 
       const result = await getuseid.json();
       const userid = JSON.stringify(result.data).slice(1, -1);
+      console.log(`User id: ${userid}`);
 
       const sendreserve = await fetch(formend, {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${token}`,
-          Accept: "application/json",
-          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`,
+          "Accept" : "application/json",
+          "Content-Type": "application/json"
         },
         body: JSON.stringify({
           reservation_status: true,
           villaId: id,
           userId: userid,
-          name: formData.name,
-          phoneNumber: formData.phoneNumber,
-          notes: formData.notes,
-        }),
-      });
+          message: formData.message,
+        })
+      })
 
       if (!sendreserve.ok) {
-        throw new Error(`Error submitting form: ${sendreserve.status}`);
+        throw new Error(`Error submitting form: ${sendreserve.status}`)
       }
 
-      console.log("Form submitted successfully");
-    } catch (error) {
+      const resresult = await sendreserve.json();
+      console.log("Form submitted successfully", resresult);
+
+    }
+
+    catch (error) {
       console.error("Error occurred submitting form: ", error);
     }
+    
   };
 
   return (
-    <form onSubmit={handleSubmit} className="reservation">
-      <div className="form-row">
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className="form-row">
-        <label htmlFor="phoneNumber">Phone Number:</label>
-        <input
-          type="tel"
-          id="phoneNumber"
-          name="phoneNumber"
-          value={formData.phoneNumber}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className="form-row">
-        <label htmlFor="notes">Notes:</label>
-        <textarea
-          id="notes"
-          name="notes"
-          value={formData.notes}
-          onChange={handleChange}
-          placeholder="Add any additional notes or special requests..."
-          required
-        />
+    <form onSubmit={handleSubmit}
+    className="reservation">
+      <div>
+        <label>
+          Message:
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+          />
+        </label>
       </div>
       <button type="submit">Submit</button>
     </form>
